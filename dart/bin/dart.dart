846d6ba7a648196b2b133cfb676aa3aa915f49e6
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-01-04 07:59:14 +0600

    variable, data types

--- dart/bin/dart.dart
+++ dart/bin/dart.dart
@@ -1,4 +1,17 @@
 void main(){
-  print("hello World");
-  print("hello World");
+  //variable
+  var x = 10;
+  var y =39;
+  var r = x+y;
+  // print(r);
+
+  //dart list
+  var city = [1,2,3,4, "a", "b"];
+
+  for(var i = 0; i< city.length; i++){
+    if(i==2){
+      continue;
+    }
+    print(city[i]);
+  }
 }
