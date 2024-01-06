01ca01d8bd9f51bab36f0ddc055ca9231e3e85d8
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-01-06 20:38:43 +0600

    function

--- dart/bin/dart.dart
+++ dart/bin/dart.dart
@@ -6,14 +6,16 @@ import 'sets.dart';
 import 'variable.dart';
 
 void main() {
-// variable();
-// print('\n');
-// list();
-// print('\n');
-// map();
-// print('\n');
-// sets();
-// loop();
+variable();
+print('\n');
+list();
+print('\n');
+map();
+print('\n');
+sets();
+print('\n');
+loop();
+print('\n');
 function();
 }
 
--- dart/bin/function.dart
+++ dart/bin/function.dart
@@ -1,10 +1,9 @@
-function(){
+function() {
   print("-------------------- FUNCTION --------------------");
-  var result = addTwo(2,2);
+  var result = addTwo(2, 2);
   print(result);
-
 }
 
-int addTwo(int x, int y){
-  return x+y;
+int addTwo(int x, int y) {
+  return x + y;
 }
