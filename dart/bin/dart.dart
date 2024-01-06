0ce941592c550e44fcc63d65fe9af3495c8f6d45
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-01-06 13:32:55 +0600

    conflict resolved

--- dart/bin/dart.dart
+++ dart/bin/dart.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'function.dart';
 import 'list.dart';
 import 'loop.dart';
@@ -7,48 +6,15 @@ import 'sets.dart';
 import 'variable.dart';
 
 void main() {
-  // variable();
-  // print('\n');
-  // list();
-  // print('\n');
-  // map();
-  // print('\n');
-  // sets();
-  // loop();
-  function();
-=======
-void main(){
-  //variable
-  print("Simple Variable");
-  var x = 10;
-  var y =39;
-  var r = x+y;
-  print(r);
-  print("___________________");
-
-
-  print("List");
-  /*Dart list
-  List is a data type which is the concept of Array
-  */
-  var city = [1,2,3,4, "a", "b"];
-  for(var i = 0; i< city.length; i++){
-    if(i==2){
-      continue;
-    }
-    print(city[i]);
-  }
-  print("___________________");
-
-  /*
-  * Dart map are equiavalent to Object
-  */
-
-  var map = {"name": "Me", "age": 20};
-  print(map["name"]);
-
-
->>>>>>> ed2f2f5ec5a1c6f22912e6b06cb868b91d6f6feb
+// variable();
+// print('\n');
+// list();
+// print('\n');
+// map();
+// print('\n');
+// sets();
+// loop();
+function();
 }
 
 
